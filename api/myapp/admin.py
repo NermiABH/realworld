88f0fa95812ee6415aac7d3e279785abbfe84dfe
NermiABH <swapsanssmyr@gmail.com>
2022-09-24 23:07:36 +0300

    Add CustomUser Model

--- api/myapp/admin.py
+++ api/myapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'is_staff', 'is_active',)
+    list_filter = ('email', 'is_staff', 'is_active',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password',
+                           'username', 'bio', 'image',
+                           'data_of_birth', 'subscriptions', 'date_of_joining',
+                           'subscribers', )}),
+        ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    )
+    readonly_fields = ('date_of_joining', )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- api/myapp/models.py
+++ api/myapp/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+from .managers import CustomUserManager
+MAX_LENGTH = 100
 
-# Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('Email address'), unique=True)
+    username = models.CharField(_('Username'), unique=True, max_length=MAX_LENGTH)
+    bio = models.TextField(_('Biography'), blank=True)
+    image = models.ImageField(_('Image'), null=True)
+    date_of_joining = models.DateTimeField(_('Date joined'), auto_now_add=True)
+    data_of_birth = models.DateField(_('Date of birth'), blank=True, null=True )
+    subscriptions = models.ManyToManyField('self',  symmetrical=False,
+                                           verbose_name=_('Subscriptions'),
+                                           blank=True,
+                                           related_name='subscription')
+    subscribers = models.ManyToManyField('self',  symmetrical=False,
+                                         verbose_name=_('Subscribers'),blank=True, related_name='subscriber')
+    # favourites = models.ManyToManyField('Article', verbose_name=_('Favorite articles'), related_name=)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+# class Article(models.Model):
+#     title = models.CharField(_('Title'), max_length=MAX_LENGTH)
+#     description = models.TextField(_('Description'))
+#     body = models.TextField(_('Text'))
+#     created = models.DateTimeField(_('Creation date'), auto_now_add=True)
+#     updated = models.DateTimeField(_('Update date'), auto_now=True)
+#     author = models.ForeignKey(CustomUser, _('Author'), models.CASCADE)
+#     slug = models.SlugField(CustomUser, _('Slug'), models.CASCADE)
+#
+#
+# class Comments(models.Model):
+#     author = models.ForeignKey(CustomUser, _('Author'), models.CASCADE)
+#     body = models.TextField(_('Body'))
+#     likes = models.Count(_('Number of likes'))
+#     dislikes = models.Count(_('Number of dislikes'))
+#     reply = models.ForeignKey('self', , models.CASCADE)
