0a67602ee7e09e04551b8eee93a984be9e70588f
NermiABH <swapsanssmyr@gmail.com>
2022-10-14 21:15:59 +0300

    .

--- config/settings.py
+++ config/settings.py
@@ -141,7 +141,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 20,
-    # 'EXCEPTION_HANDLER': 'myapp.views.exception_handler',
+    'EXCEPTION_HANDLER': 'myapp.views.exception_handler',
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ]
